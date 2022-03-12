45b9ab49f559b9926c74bb1ccab193e83c00b468
kerron <kerron68@gmail.com>
2022-03-12 18:28:21 +0000

    feat: add first algo - adjacent_difference

---
+++ src/lib.ts
@@ -0,0 +1,32 @@
+/**
+ * Computes the difference between the second and first of each adjacent pair of elements
+ * of the range [first, last] and returns the result.
+ * @param first start of range
+ * @param last end of range
+ * @param v list of elements to process
+ * @param operation optional binary function that will be applied to elements
+ * @returns array of item
+ */
+export const adjacent_difference = (
+  first: number,
+  last: number,
+  v: number[],
+  operation?: (a: number, b: number) => number
+): number[] => {
+  if (!v.length) return [];
+  const diff = (v: number, i: number, arr: number[]) =>
+    i === 0 ? i : v - arr[i - 1];
+  const fn = operation || diff;
+  return v.slice(first, last).map(fn);
+};
+/**
+ * Takes two numbers as input and calculate the sum
+ * @param a number
+ * @param b number
+ * @returns addition of a and b
+ */
+const add = (a: number, b: number) => a + b;
+
+console.log(adjacent_difference(0, 5, [], add)); // []
+console.log(adjacent_difference(0, 5, [1, 9, 3, 4, 5, 6])); // [ 0, 8, -6, 1, 1 ]
+console.log(adjacent_difference(0, 5, [1, 9, 3, 4, 5, 6], add)); // [ 1, 10, 5, 7, 9 ]
