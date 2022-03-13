d504205af88a8b4377a16a421c22006a049a8fb2
kerron <kerron68@gmail.com>
2022-03-13 12:22:14 +0000

    feat: remove console logs

--- src/lib.ts
+++ src/lib.ts
@@ -33,14 +33,4 @@ export const adjacent_difference = (
   const fn = operation || diff;
   return v.slice(first, last).map(fn);
 };
-/**
- * Takes two numbers as input and calculate the sum
- * @param a number
- * @param b number
- * @returns addition of a and b
- */
-const add = (a: number, b: number) => a + b;
 
-console.log(adjacent_difference(0, 5, [], add)); // []
-console.log(adjacent_difference(0, 5, [1, 9, 3, 4, 5, 6])); // [ 0, 8, -6, 1, 1 ]
-console.log(adjacent_difference(0, 5, [1, 9, 3, 4, 5, 6], add)); // [ 1, 10, 5, 7, 9 ]
