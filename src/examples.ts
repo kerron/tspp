556ebb2c2769cbb31e025d78ff99dda80ada6ac4
Kerron Parchment <kerron68@gmail.com>
2022-03-14 17:21:27 +0000

    feat: update example file

--- src/examples.ts
+++ src/examples.ts
@@ -1,4 +1,4 @@
-import { adjacent_difference, adjacent_find, greater, lesser } from "./lib";
+import { adjacent_difference, adjacent_find, greater, lesser } from "./index";
 
 /**
  * Takes two numbers as input and calculate the sum
@@ -8,7 +8,8 @@ import { adjacent_difference, adjacent_find, greater, lesser } from "./lib";
  */
 const add = (a: number, b: number) => a + b;
 
-// console.log(adjacent_difference(0, 5, [], add)); // []
-// console.log(adjacent_difference(0, 5, [1, 9, 3, 4, 5, 6])); // [ 0, 8, -6, 1, 1 ]
-// console.log(adjacent_difference(0, 5, [1, 9, 3, 4, 5, 6], add)); // [ 1, 10, 5, 7, 9 ]
-console.log(adjacent_find(0, 4, [0, 1, 2, 4, 3, 2, 4, 4], lesser));
+console.log(adjacent_difference(0, 5, [], add)); // []
+console.log(adjacent_difference(0, 5, [1, 9, 3, 4, 5, 6])); // [ 0, 8, -6, 1, 1 ]
+console.log(adjacent_difference(0, 5, [1, 9, 3, 4, 5, 6], add)); // [ 1, 10, 5, 7, 9 ]
+console.log(adjacent_find(0, 4, [0, 1, 2, 4, 3, 2, 4, 4], lesser)); // 1
+console.log(adjacent_find(0, 4, [0, 1, 2, 4, 3, 2, 4, 4], greater)); // 4
