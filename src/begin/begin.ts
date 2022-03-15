4403d9aec59d4ecc519174158b2d80ba4544cd56
Kerron Parchment <kerron68@gmail.com>
2022-03-15 22:20:22 +0000

    feat/2: add begin and end methods (#3)

---
+++ src/begin/begin.ts
@@ -0,0 +1,5 @@
+/**
+ * returns an iterator to the beginning of the given range (i.e. the first element).
+ * @returns a pointer to the beginning of the array
+ */
+export const begin = () => 0;
---
+++ src/end/end.ts
@@ -0,0 +1,6 @@
+/**
+ * returns a pointer to the end (i.e. the element after the last element) of the given list.
+ * @param list 	an array of arbitrary type
+ * @returns a pointer to the end of the array
+ */
+export const end = <T>(list: T[]) => list.length;
