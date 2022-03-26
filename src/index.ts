405eb9d8891ec22b5e3b4e8a6adcef5005cf10c6
kerron <kerron68@gmail.com>
2022-03-26 11:49:56 +0000

    feat!: rename to adjacentDifference

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import { adjacent_difference } from "./adjacent_difference/adjacent_difference";
+import { adjacentDifference } from "./adjacentDifference/adjacentDifference";
 import { adjacent_find } from "./adjacent_find/adjacent_find";
 import { greater } from "./greater/greater";
 import { lesser } from "./lesser/lesser";
 
-export { adjacent_difference, adjacent_find, lesser, greater };
+export { adjacentDifference, adjacent_find, lesser, greater };
