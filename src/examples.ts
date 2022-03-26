35b77f7f1fc4d843538aaa9de00592a4d0a4a40c
Kerron Parchment <kerron68@gmail.com>
2022-03-26 11:44:15 +0000

    update examples (#8)

--- src/examples.ts
+++ src/examples.ts
@@ -1,4 +1,8 @@
+import { allOf } from "./allOf/allOf";
+import { begin } from "./begin/begin";
+import { end } from "./end/end";
 import { adjacent_difference, adjacent_find, greater, lesser } from "./index";
+import { isEven } from "./isEven/isEven";
 
 /**
  * Takes two numbers as input and calculate the sum
@@ -8,8 +12,11 @@ import { adjacent_difference, adjacent_find, greater, lesser } from "./index";
  */
 const add = (a: number, b: number) => a + b;
 
-console.log(adjacent_difference(0, 5, [], add)); // []
-console.log(adjacent_difference(0, 5, [1, 9, 3, 4, 5, 6])); // [ 0, 8, -6, 1, 1 ]
-console.log(adjacent_difference(0, 5, [1, 9, 3, 4, 5, 6], add)); // [ 1, 10, 5, 7, 9 ]
-console.log(adjacent_find(0, 4, [0, 1, 2, 4, 3, 2, 4, 4], lesser)); // 1
-console.log(adjacent_find(0, 4, [0, 1, 2, 4, 3, 2, 4, 4], greater)); // 4
+// console.log(adjacent_difference(0, 5, [], add)); // []
+// console.log(adjacent_difference(0, 5, [1, 9, 3, 4, 5, 6])); // [ 0, 8, -6, 1, 1 ]
+// console.log(adjacent_difference(0, 5, [1, 9, 3, 4, 5, 6], add)); // [ 1, 10, 5, 7, 9 ]
+// console.log(adjacent_find(0, 4, [0, 1, 2, 4, 3, 2, 4, 4], lesser)); // 1
+// console.log(adjacent_find(0, 4, [0, 1, 2, 4, 3, 2, 4, 4], greater)); // 4
+
+const list = [1, 2, 6, 4, 8];
+console.log("allOf", allOf(begin(), end(list), list, isEven)); // 4
